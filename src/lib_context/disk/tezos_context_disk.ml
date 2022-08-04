7d3d8e6056f0d11c3a31bbb1bd458caba160fe18
ngoguey42 <ngoguey42@free.fr>
2022-08-04 11:41:49 +0200

    Lib_context: Hot-wire recording shim into disk context

--- src/lib_context/disk/tezos_context_disk.ml
+++ src/lib_context/disk/tezos_context_disk.ml
@@ -53,4 +53,71 @@
 module Context_binary = Context.Make (Tezos_context_encoding.Context_binary)
 
 (** The context of a tezos node. Persisted to disk. *)
-module Context = Context.Make (Tezos_context_encoding.Context)
+module Context = struct
+
+  let make_wrapped_context () :
+      (module Tezos_context_disk_sigs.TEZOS_CONTEXT_UNIX) =
+    let module M = struct
+      module Impl :
+        Tezos_context_disk_sigs.TEZOS_CONTEXT_UNIX =
+        Context.Make (Tezos_context_encoding.Context)
+
+      module type RECORDER =
+        Tezos_context_recording.Recorder.S with module Impl = Impl
+    end in
+    let raw_actions_recorder : (module M.RECORDER) option =
+      match Tezos_context_helpers.Env.(v.record_raw_actions_trace) with
+      | `No -> None
+      | `Yes prefix ->
+          (* Create a raw actions trace file in the [prefix] directory for the
+             current process. Multiple processes will record to their own files.
+          *)
+          Some
+            (module Tezos_context_recording.Raw_actions_trace_recorder.Make
+                      (M.Impl)
+                      (struct
+                        let prefix = prefix
+                      end))
+    in
+    let stats_recorder : (module M.RECORDER) option =
+      match Tezos_context_helpers.Env.(v.record_stats_trace) with
+      | `No -> None
+      | `Yes prefix ->
+          (* The stats trace file will be created during the first call to [init].
+             Multiple processes will record to their own files. *)
+          Some
+            (module Tezos_context_recording.Stats_trace_recorder.Make
+                      (M.Impl)
+                      (struct
+                        let prefix = prefix
+
+                        let message = Tezos_context_helpers.Env.(v.stats_trace_message)
+                      end))
+    in
+    (module Tezos_context_recording.Shim.Make
+              (M.Impl)
+              (struct
+                module type RECORDER = M.RECORDER
+
+                let l =
+                  Option.to_list raw_actions_recorder
+                  @ Option.to_list stats_recorder
+              end))
+
+  include
+    (val match Tezos_context_helpers.Env.(v.record_raw_actions_trace, v.record_stats_trace) with
+         | (`No, `No) ->
+             (module Context.Make (Tezos_context_encoding.Context)
+             : Tezos_context_disk_sigs.TEZOS_CONTEXT_UNIX)
+         | (`Yes _, _) | (_, `Yes _) ->
+             (* Enable recording for one or both of the trace kind.
+                Seen from outside of [Tezos_context] these recordings are
+                seemless, except for:
+                - logging of important informations,
+                - exceptions on IO error,
+                - the creation of files that will be closed [at_exit].
+             *)
+             make_wrapped_context ())
+
+
+end
