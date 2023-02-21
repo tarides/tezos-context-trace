62ee6ddbd6a83f676cbbce3e43e142e562d32279
Adarsh Amirtham <adarsh@tarides.com>
2023-02-21 12:17:13 +0100

    guix: Use the package definition from the guix-tarides channel.
    This includes a required patch on Tezos.

--- guix.scm
+++ guix.scm
@@ -1,36 +1,4 @@
 (use-modules
- (guix packages)
- (guix build-system dune)
- (guix git)
- (guix git-download)
- ((guix licenses) #:prefix license:)
- (gnu packages admin)
- (gnu packages ocaml)
- (tarides packages ocaml)
- (tarides packages irmin)
  (tarides packages tezos))
 
-(define ocaml-tezos-context-replay
-  (package-with-tezos-16
-   (package
-     (name "ocaml-tezos-context-replay")
-     (version "0.0.0")
-     (home-page "https://github.com/adatario/tezos-context-replay")
-     (source (git-checkout (url (dirname (current-filename)))))
-     (build-system dune-build-system)
-     (propagated-inputs
-      (list
-
-       ;; tezos-contest (aka lib_context)
-       ocaml-tezos-context
-
-       ;; Extra dependencies required by the replay patches
-       ocaml-ppx-deriving
-       ocaml-ppx-deriving-yojson
-       ocaml-printbox
-       ocaml-bentov))
-     (synopsis #f)
-     (description #f)
-     (license license:isc))))
-
-ocaml-tezos-context-replay
+ocaml-tezos-context-trace
